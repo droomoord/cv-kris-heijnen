6fc696dff0f7e8c915386782247b3fc10b172fd7
Kris <heijnenkris@gmail.com>
2020-09-10 17:00:46 +0200

    v2

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const boxes = document.querySelectorAll(".box");
+const toggelTextButtons = document.querySelectorAll(
+  ".fa-minus-square, .fa-plus-square"
+);
 
-let options = {
-    rootMargin: '0px',
-    threshold: 1.0
-  }
-
-observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("slide-in");
-    }
+toggelTextButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    toggleRespeciveText(button);
   });
-}, options);
-
-boxes.forEach((box) => {
-  observer.observe(box);
 });
+
+function toggleRespeciveText(element) {
+  const respectiveText = element.parentElement.parentElement.children[1];
+  if (element.classList.contains("fa-minus-square")) {
+    respectiveText.classList.remove("show-text");
+    respectiveText.classList.add("hide-text");
+    element.classList.remove("fa-minus-square");
+    element.classList.add("fa-plus-square");
+  } else {
+    respectiveText.classList.remove("hide-text");
+    respectiveText.classList.add("show-text");
+    element.classList.remove("fa-plus-square");
+    element.classList.add("fa-minus-square");
+  }
+}
+
+// const boxes = document.querySelectorAll(".box");
+
+// let options = {
+//   rootMargin: "200px",
+//   threshold: 1.0,
+// };
+
+// observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("fade-in");
+//     }
+//   });
+// }, options);
+
+// boxes.forEach((box) => {
+//   observer.observe(box);
+// });
